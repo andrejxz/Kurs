20797e3519e0da2decae7435d7bc8a564d603c31
Konstantin Kokhno <kkohno@mail.ru>
2016-11-09 14:21:52 +0700

    все заного

--- Kurs/MainForm.cs
+++ Kurs/MainForm.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace Kurs
 {
-    public partial class Form4 : Form
-    {
-        public Form4()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
 }
