c0ebe808eb01272e34277c93a799a706c2e86d78
Konstantin Kokhno <kkohno@mail.ru>
2016-11-12 14:33:09 +0700

    применили ENtityFramework

--- Kurs/MainForm.cs
+++ Kurs/MainForm.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Model;
 
 namespace Kurs
 {
 	public partial class MainForm : Form
 	{
+		DBContext context = new DBContext();
 		public MainForm()
 		{
 			InitializeComponent();
+			context.GoodCategories.Load();
+			goodCategoryBindingSource.DataSource = context.GoodCategories.Local.ToBindingList();
+		}
+
+		private void goodCategoryBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			goodCategoryBindingSource.EndEdit();
+			Validate();
+			context.SaveChanges();
 		}
 	}
 }
---
+++ Model/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+	/// <summary>
+	/// контекст БД
+	/// </summary>
+	public class DBContext : DbContext
+	{
+		public DbSet<GoodCategory> GoodCategories { get; set; }
+		public DbSet<Good> Goods { get; set; }
+		public DbSet<StoreItem> StoreItems { get; set; }
+		public DbSet<SaleItem> SaleItem { get; set; }
+
+		public DBContext() : base("ConnectionString")
+		{
+		}
+	}
+}
